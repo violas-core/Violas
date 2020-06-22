3c7651c70f9cf365b6a1d239b5437ffa56bbf9c7
HunterSun2018 <sht@palliums.org>
2020-06-22 15:49:41 +0800

    change cli prompt from Libra% to Violas%

--- testsuite/cli/src/main.rs
+++ testsuite/cli/src/main.rs
@@ -146,7 +146,7 @@ fn main() {
         .build();
     let mut rl = Editor::<()>::with_config(config);
     loop {
-        let readline = rl.readline("libra% ");
+        let readline = rl.readline("Violas% ");
         match readline {
             Ok(line) => {
                 let params = parse_cmd(&line);
